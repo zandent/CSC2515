f78f7d15d8fcdbac9349046fc6e1187003720bb5
zandent <simon.zhao@mail.utoronto.ca>
2020-09-14 00:06:27 -0400

    update

--- hw1/q1b.py
+++ hw1/q1b.py
@@ -1,8 +1,14 @@
 from scipy import integrate
-def expect():
+def q1a_mean():
     y0 = lambda x: x
     y1 = lambda x: x**2
     return 2*integrate.quad(y1,0,1)[0] - 2*(integrate.quad(y0,0,1)[0])**2
+def q1a_var():
+    y0 = lambda x: x
+    y1 = lambda x: x**2
+    y2 = lambda x: x**3
+    y3 = lambda x: x**4
+    return 2*integrate.quad(y3,0,1)[0] - 2*4*integrate.quad(y2,0,1)[0]*integrate.quad(y0,0,1)[0] + 6*integrate.quad(y1,0,1)[0]**2 - q1a_mean()**2
 
-print(expect())
+print(q1a_mean(), q1a_var())
 
