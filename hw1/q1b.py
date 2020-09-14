0e3f76526e1dd76107338a6e7fbc874d18ef1966
zandent <simon.zhao@mail.utoronto.ca>
2020-09-13 22:15:34 -0400

    add q1b

--- hw1/q1b.py
+++ hw1/q1b.py
@@ -1,8 +1,8 @@
 from scipy import integrate
 def expect():
     y0 = lambda x: x
-    y1 = lambda x: x*2
-    return 2*integrate.quad(y1,0,1) - 2*2*integrate.quad(y0,0,1)
+    y1 = lambda x: x**2
+    return 2*integrate.quad(y1,0,1)[0] - 2*(integrate.quad(y0,0,1)[0])**2
 
 print(expect())
 
