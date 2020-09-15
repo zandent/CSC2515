8d9c029b6bf11b602c612c805ee5a940f29a5f1f
zandent <simon.zhao@mail.utoronto.ca>
2020-09-15 15:18:19 -0400

    rename to q1a

--- hw1/q1a.py
+++ hw1/q1a.py
@@ -9,15 +9,7 @@ def q1a_var():
     y2 = lambda x: x**3
     y3 = lambda x: x**4
     return 2*integrate.quad(y3,0,1)[0] - 2*4*integrate.quad(y2,0,1)[0]*integrate.quad(y0,0,1)[0] + 6*integrate.quad(y1,0,1)[0]**2 - q1a_mean()**2
-def q1b_mean(d):
-    return q1a_mean()*d
-def q1b_var(d):
-    return q1a_var()*d
 
-#question 1a answer
 print(q1a_mean(), q1a_var())
 
-#question 1b answer
-d = 10 
-print(q1b_mean(d), q1b_var(d))
 
