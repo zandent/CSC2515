d2471fc6da29ed2c4a60478e383e5613d0cd318c
zandent <simon.zhao@mail.utoronto.ca>
2020-12-10 21:39:59 -0500

    update final version

--- rating_prediction/main.py
+++ rating_prediction/main.py
@@ -1,47 +1,9 @@
-# import graphlab
-# from graphlab import SFrame
-# product_reviews=graphlab.SFrame.read_json('data/train.json', orient='lines')
-# # print (product_reviews.head())
-# # product_reviews.print_rows()
-# product_reviews['overall'] = [int(i) for i in product_reviews['overall']]
-# product_reviews['wordcount'] = graphlab.text_analytics.count_words(product_reviews['reviewText']+product_reviews['summary'])
-# vs_reviews = graphlab.SFrame.read_json('data/test.json', orient='lines')
-# vs_reviews['wordcount'] = graphlab.text_analytics.count_words(vs_reviews['reviewText']+vs_reviews['summary'])
-# vs_reviews.print_rows()
-
-# # product_reviews = product_reviews[product_reviews['overall'] !=3]
-# # product_reviews['posrating'] = product_reviews['overall'] >= 4
-# product_reviews = product_reviews.dropna()
-# train_data, test_data = product_reviews.random_split(0.8, seed=0)
-# sentiment_model = graphlab.logistic_classifier.create (train_data, target='overall', features=['wordcount'],validation_set=test_data, max_iterations=100)
-# sentiment_model.evaluate(test_data, metric='roc_curve')
-# # print sentiment_model
-
-
-# results=sentiment_model.predict_topk(test_data, output_type='probability', k=1)
-# results['class'] = [float(i) for i in results['class']]
-# test_data.add_columns(results)
-# test_data=test_data.sort('overall', ascending=False)
-# # print results
-# print test_data
-# print test_data.tail()
-
-# # results=sentiment_model.predict_topk(vs_reviews, output_type='probability', k=1)
-# # results['class'] = [float(i) for i in results['class']]
-# # vs_reviews.add_columns(results)
-# # vs_reviews=vs_reviews.sort('class', ascending=False)
-# # # print results
-# # print vs_reviews
-# # print vs_reviews.tail()
-
-# LSTM for sequence classification in the IMDB dataset
 import numpy
 from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
-from keras.layers import Bidirectional
-from keras.layers import LSTM,RNN,LSTMCell,SimpleRNNCell,GRU
+from keras.layers import GRU
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 from keras import backend as K
@@ -157,10 +119,10 @@ y_train = numpy.array(y_train)
 # print ("idx is", idx[100])
 # print ("Xcmp is", X_cmp[idx[100]])
 # print ("ycmp is", y_cmp[idx[100]])
-# (_, X_test) = list_splitter(X_train, 0.8)
-# (_, y_test) = list_splitter(y_train, 0.8)
-(X_train, X_test) = list_splitter(X_train, 0.8)
-(y_train, y_test) = list_splitter(y_train, 0.8)
+(_, X_test) = list_splitter(X_train, 0.8)
+(_, y_test) = list_splitter(y_train, 0.8)
+# (X_train, X_test) = list_splitter(X_train, 0.8)
+# (y_train, y_test) = list_splitter(y_train, 0.8)
 # truncate and pad input sequences
 max_review_length = 500
 X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
@@ -173,12 +135,7 @@ X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
 # create the model
 model = Sequential()
 model.add(Embedding(top_words, 32, embeddings_initializer = 'zeros',input_length=max_review_length))
-# 4 different cells: default rnncell/GRU/LSTM/self-defined rnncell
-# model.add(RNN(SimpleRNNCell(32)))
-#model.add(RNN(MinimalRNNCell(32)))
 model.add(GRU(32))
-# model.add(Bidirectional(GRU(32)))
-# model.add(LSTM(32))
 model.add(Dropout(0.1))
 # model.add(Dense(250, activation='relu'))
 model.add(Dense(6, activation='sigmoid'))
@@ -188,8 +145,8 @@ print(model.summary())
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1)
 history = model.fit(X_train, y_train, epochs=5, batch_size=64,validation_data = (X_test,y_test),callbacks=[es])
 # Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+# scores = model.evaluate(X_test, y_test, verbose=0)
+# print("Accuracy: %.2f%%" % (scores[1]*100))
 # list all data in history
 print(history.history.keys())
 # summarize history for accuracy
